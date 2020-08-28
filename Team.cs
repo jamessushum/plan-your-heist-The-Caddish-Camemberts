bde2e976e77ed40a47ed0b0cc470e9410fe9b99f
Patrick Murphy <engine_ear@mac.com>
2020-08-28 13:52:33 -0500

    Phase 3 complete. Bank at 100. We always go to jail

--- Team.cs
+++ Team.cs
@@ -1,31 +1,37 @@
 using System;
 using System.Collections.Generic;
 
-namespace plan_your_heist_THE_CADDISH_CAMEMBERTS
-{
-    public class Team
-    {
-        public Dictionary<string, TeamMember> TeamList = new Dictionary<string, TeamMember>();
+namespace plan_your_heist_THE_CADDISH_CAMEMBERTS {
+    public class Team {
+        public Dictionary<string, TeamMember> TeamList = new Dictionary<string, TeamMember> ();
 
-        public void AddMember(TeamMember member)
-        {
-            TeamList.Add(member.memberName, member);
+        public void AddMember (TeamMember member) {
+            TeamList.Add (member.memberName, member);
         }
-        public void DisplayTeamMembers()
-        {
-            DisplayCount();
-            foreach (KeyValuePair<string, TeamMember> teamMember in TeamList)
-            {
-                Console.WriteLine($"Name: {teamMember.Key}");
-                Console.WriteLine($"Skill: {teamMember.Value.skillLevel}");
-                Console.WriteLine($"Courage: {teamMember.Value.courageFactor}");
-                Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        public void DisplayTeamMembers () {
+            DisplayCount ();
+            foreach (KeyValuePair<string, TeamMember> teamMember in TeamList) {
+                Console.WriteLine ($"Name: {teamMember.Key}");
+                Console.WriteLine ($"Skill: {teamMember.Value.skillLevel}");
+                Console.WriteLine ($"Courage: {teamMember.Value.courageFactor}");
+                Console.WriteLine ("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
             }
 
         }
-        public void DisplayCount()
+        public void DisplayCount () {
+            Console.WriteLine ($"Team Count: {TeamList.Count}");
+        }
+
+        public int AddTeamSkills ()
+
         {
-            Console.WriteLine($"Team Count: {TeamList.Count}");
+            int TotalSkills = 0;
+
+            foreach (KeyValuePair<string, TeamMember> member in TeamList) {
+                TotalSkills += member.Value.skillLevel;
+            }
+
+            return TotalSkills;
         }
     }
 }
