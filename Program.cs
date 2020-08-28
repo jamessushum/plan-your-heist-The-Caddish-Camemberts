e074825dd5f378fe8d978da66565d52fbcb7d24f
James Su-Shum <sushumjames@gmail.com>
2020-08-28 12:32:32 -0400

    Phase 1 complete

--- Program.cs
+++ Program.cs
@@ -4,9 +4,72 @@ namespace plan_your_heist_THE_CADDISH_CAMEMBERTS
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Clear();
+            Console.WriteLine("Plan Your Heist!");
+            Console.WriteLine("----------------");
+
+            start();
+
+        }
+        public static void start()
+        {
+            // Get member name
+            Console.WriteLine("Enter a team member's name: ");
+            string memberName = Console.ReadLine();
+
+            // Get member skill level
+            int skillLevel;
+            do
+            {
+                skillLevel = enterSkillLevel(memberName);
+            }
+            while (skillLevel < 1 || skillLevel > 10);
+
+            // Get member courage factor
+            double courageFactor;
+            do
+            {
+                courageFactor = enterCourageFactor(memberName);
+            }
+            while (courageFactor < 0.0 || courageFactor > 2.0);
+
+            Console.WriteLine(memberName);
+            Console.WriteLine(skillLevel);
+            Console.WriteLine(courageFactor);
+
+        }
+
+        // Method gets user skill level string and returns integer
+        public static int enterSkillLevel(string memberName)
+        {
+            string memberSkillLevel;
+            int skillLevel;
+            do
+            {
+                Console.WriteLine($"Enter {memberName}'s skill level (between 1 to 10): ");
+                memberSkillLevel = Console.ReadLine();
+            }
+            while ((int.TryParse(memberSkillLevel, out skillLevel) == false));
+
+            return skillLevel;
+        }
+
+        // Method gets user courage factor string and return integer
+        public static double enterCourageFactor(string memberName)
+        {
+            string memberCourageFactor;
+            double courageFactor;
+            do
+            {
+                Console.WriteLine($"Enter {memberName}'s courage factor (between 0.0 to 2.0): ");
+                memberCourageFactor = Console.ReadLine();
+            }
+            while ((double.TryParse(memberCourageFactor, out courageFactor) == false));
+
+            return courageFactor;
         }
     }
 }
---
+++ TeamMember.cs
@@ -0,0 +1,9 @@
+namespace plan_your_heist_THE_CADDISH_CAMEMBERTS
+{
+  public class TeamMember
+  {
+    public string memberName { get; set; }
+    public int skillLevel { get; set; }
+    public double courageFactor { get; set; }
+  }
+}
